5e7b807f7107cc704368d4aab4b094b13c517bb4
Diane Mpacko <diane.mpacko@gmail.com>
2020-08-16 14:40:48 +0200

    project section done. upcoming css style

--- src/Components/Dashboard/Sections/Knowledge.js
+++ src/Components/Dashboard/Sections/Knowledge.js
@@ -10,8 +10,46 @@ class Knowledge extends Component {
     
     render() {
         return (
-            <div>
-                <p>Knowledge</p>
+            <div className="render-infos">
+               <form>
+                    <div className="form-group row">
+                        <div className="col-sm-4">Environnement de travail</div>
+                        <div className="col-sm-8">
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox" id="gridCheck1" />
+                                <label className="form-check-label" htmlFor="gridCheck1">
+                                Windows
+                                </label><br />
+                                <input className="form-check-input" type="checkbox" id="gridCheck1" />
+                                <label className="form-check-label" htmlFor="gridCheck1">
+                                Linux/Ubuntu
+                                </label><br />
+                                <input className="form-check-input" type="checkbox" id="gridCheck1" />
+                                <label className="form-check-label" htmlFor="gridCheck1">
+                                MAC OS
+                                </label><br />
+                                <div style={{display: "flex"}}>
+                                    <input className="form-check-input" type="checkbox" id="gridCheck1" />
+                                    <input type="text" className="form-control" style={{ width: "200px"}} placeholder="Autre"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="form-group row">
+                        <label className="col-sm-4 col-form-label">Technos et outils</label>
+                        <div className="col-sm-8">
+                        <textarea className="form-control" style={{height: "100px"}} placeholder="Séparez par une virgule"/>
+                        </div>
+                    </div>
+
+                    <div className="form-group row">
+                        <label className="col-sm-4 col-form-label">Mots-clés</label>
+                        <div className="col-sm-8">
+                        <textarea className="form-control" style={{height: "100px"}} placeholder="Séparez par une virgule"/>
+                        </div>
+                    </div>
+               </form>
             </div>
         )
     }
--- src/Components/Dashboard/Sections/Projects.js
+++ src/Components/Dashboard/Sections/Projects.js
@@ -4,13 +4,41 @@ class Projects extends Component {
     constructor(){
         super()
         this.state = {
-            bu:"top"
+            visible: false
         }
     }
+
+    showForm = () => {
+
+        this.setState(prevState => ({
+            visible: !prevState.visible
+        })
+        )
+
+    }
+
     render() {
         return (
-            <div>
-                <p>Projects</p>
+            <div className="render-infos">
+                <div style={{ display: "flex", justifyContent: "flex-end"}}>
+                    <button className="btn" onClick={this.showForm}>Nouveau projet</button>
+                </div>
+
+                <div className="form-group row new-project" visible={this.state.visible} style={{display: this.state.visible ? 'flex' : 'none'}}>
+                    <div className="custom-file align-x" style={{ width: "100%"}}>
+                        <div className="col-sm-8">
+                            <input type="text" className="form-control" placeholder="Titre du projet"/>
+                            <textarea className="form-control" style={{height: "100px"}} placeholder="Description du projet"/>
+                            <input type="file" className="custom-file-input" id="customFile" />
+                            <div style={{ display: "flex", justifyContent: "flex-end"}}><button className="btn">Ajouter</button></div>
+                            
+                        </div>
+                    </div>
+                </div>
+
+                <div className="render-projects">
+
+                </div>
             </div>
         )
     }
