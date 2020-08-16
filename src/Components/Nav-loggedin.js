f74b3546b2bc28cb38ca19968d9f1cc7b6f289c0
Diane Mpacko <diane.mpacko@gmail.com>
2020-08-16 16:36:34 +0200

    logo linked to home page

--- src/Components/Nav-loggedin.js
+++ src/Components/Nav-loggedin.js
@@ -1,13 +1,14 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Logo from "../img/logo.png";
 import Icon from "../img/login-icon.png";
+// import Home from "./Home"
 
 export default function Nav() {
     return (
         <div className="header-nav container-fluid">
             <section className="logo-login">
-                <img alt="logo" src={Logo} className="logo"/>
+                <Link to={"/"} style={{ height: "inherit"}} ><img alt="logo" src={Logo} className="logo"/></Link>
                 <button className="btn login-btn" style={{ backgroundColor: "inherit"}} >
                     <img alt="connexion" src={Icon} id="login-icon"/>
                     <span className="span-login">CONNEXION</span>
