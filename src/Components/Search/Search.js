1b31eacd47fb70b4bc4ec5b665f97de502a96952
Diane Mpacko <diane.mpacko@gmail.com>
2020-08-19 14:40:06 +0200

    searchbar done. search page ok

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import groupeimg from "../../img/people-2557396_1920.jpg";
+import Multiselect from "../Dashboard/Sections/Multiselect";
 import "./Search.css"
 
 export default class Search extends Component {
@@ -8,28 +9,28 @@ export default class Search extends Component {
             <div className="searchpage container">
                 <div className="row">
                     <div className="col-sm-3" style={{ minHeight: "15rem", background:"lightgrey"}}>
-                        <div><h2>Affinez votre recherche</h2></div>
+                        <div className="criteria"><h2>Affinez votre recherche</h2></div>
                         <div className="search-criteria">
                             <form>
-                                <div>
+                                <div className="criteria">
                                     <label>Type de contrat</label>
                                     <div className="form-check">
                                         <input className="form-check-input" type="checkbox" id="gridCheck1" style={{ marginRight: "5px"}} />
                                         <label className="form-check-label" htmlFor="gridCheck1">
                                         Contrat de professionnalisation
                                         </label><br />
-                                        <input className="form-check-input" type="checkbox" id="gridCheck1" style={{ marginRight: "5px"}}/>
-                                        <label className="form-check-label" htmlFor="gridCheck1">
+                                        <input className="form-check-input" type="checkbox" id="gridCheck2" style={{ marginRight: "5px"}}/>
+                                        <label className="form-check-label" htmlFor="gridCheck2">
                                         Contrat d'apprentissage
                                         </label><br />
-                                        <input className="form-check-input" type="checkbox" id="gridCheck1" style={{ marginRight: "5px"}} />
-                                        <label className="form-check-label" htmlFor="gridCheck1">
+                                        <input className="form-check-input" type="checkbox" id="gridCheck3" style={{ marginRight: "5px"}} />
+                                        <label className="form-check-label" htmlFor="gridCheck3">
                                         Stage
                                         </label>
                                     </div>
                                 </div>
-
-                                <div>
+                                    <hr />
+                                <div className="criteria">
                                     <label>Durée (en mois)</label>
                                     <div className="form-group row criterias-pdg">
                                         <select className="custom-select" size="3" multiple>
@@ -60,48 +61,49 @@ export default class Search extends Component {
                                         </select>
                                     </div>
                                 </div>
-
-                                <div>
+                                    <hr />
+                                <div className="criteria">
                                     <label>Disponibilité</label>
                                     <div className="form-check">
-                                        <input className="form-check-input" type="checkbox" id="gridCheck1" style={{ marginRight: "5px"}} />
-                                        <label className="form-check-label" htmlFor="gridCheck1">
+                                        <input className="form-check-input" type="checkbox" id="gridCheck4" style={{ marginRight: "5px"}} />
+                                        <label className="form-check-label" htmlFor="gridCheck4">
                                         Immédiate
                                         </label>
                                     </div>
                                 </div>
-
-                                <div>
+                                    <hr />
+                                <div className="criteria">
                                     <label>Mobilité</label>
+                                    <Multiselect />
                                 </div>
-                                
-                                <div>
+                                    <hr /> 
+                                <div className="criteria">
                                     <label>Environnement de travail</label>
                                     <div className="form-group row">
                                         <div className="col-sm-8">
                                             <div className="form-check">
-                                                <input className="form-check-input" type="checkbox" id="gridCheck1" style={{ marginRight: "5px"}} />
-                                                <label className="form-check-label" htmlFor="gridCheck1">
+                                                <input className="form-check-input" type="checkbox" id="gridCheck5" style={{ marginRight: "5px"}} />
+                                                <label className="form-check-label" htmlFor="gridCheck5">
                                                 Windows
                                                 </label><br />
-                                                <input className="form-check-input" type="checkbox" id="gridCheck1" style={{ marginRight: "5px"}}/>
-                                                <label className="form-check-label" htmlFor="gridCheck1">
+                                                <input className="form-check-input" type="checkbox" id="gridCheck6" style={{ marginRight: "5px"}}/>
+                                                <label className="form-check-label" htmlFor="gridCheck6">
                                                 Linux/Ubuntu
                                                 </label><br />
-                                                <input className="form-check-input" type="checkbox" id="gridCheck1" style={{ marginRight: "5px"}}/>
-                                                <label className="form-check-label" htmlFor="gridCheck1">
+                                                <input className="form-check-input" type="checkbox" id="gridCheck7" style={{ marginRight: "5px"}}/>
+                                                <label className="form-check-label" htmlFor="gridCheck7">
                                                 MAC OS
                                                 </label><br />
                                                 <div style={{display: "flex"}}>
-                                                    <input className="form-check-input" type="checkbox" id="gridCheck1" style={{ marginRight: "5px"}}/>
+                                                    <input className="form-check-input" type="checkbox" id="gridCheck8" style={{ marginRight: "5px"}}/>
                                                     <input type="text" className="form-control" style={{ width: "200px"}} placeholder="Autre" id="input-h"/>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-
-                                <div style={{ display:"flex", flexDirection:"column" }}>
+                                    <hr />
+                                <div className="criteria" style={{ display:"flex", flexDirection:"column" }}>
                                     <label>Mots-clés</label>
                                     <div className="col-sm-8">
                                         <textarea name="keywordsInput" className="form-control" style={{height: "100px"}} placeholder="Séparez par une virgule"/>
