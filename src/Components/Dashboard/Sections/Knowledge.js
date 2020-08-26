2ba6d9c6c7ffa4310a277424db6c0deea59f3533
Diane Mpacko <diane.mpacko@gmail.com>
2020-08-26 22:52:09 +0200

    quick update, tests states Knowledge component

--- src/Components/Dashboard/Sections/Knowledge.js
+++ src/Components/Dashboard/Sections/Knowledge.js
@@ -5,10 +5,9 @@ class Knowledge extends Component {
     constructor(props){
         super(props)
         this.state = {
-            // toolsInput: "",
-            // toolsArray: [],
             keywordsInput: "",
-            keywordsArray: []
+            keywordsArray: [],
+            system: []
         }
     }
 
@@ -24,6 +23,13 @@ class Knowledge extends Component {
             keywordsArray: keywordsContent 
         }); 
     }
+
+    checkedSystem = (e) => {
+        this.setState({
+            [ e.target.name]: e.target.value
+        })
+        console.log(this.state.system)
+    }
     
     render() {
         //rendu keywords
@@ -36,37 +42,27 @@ class Knowledge extends Component {
                         <div className="col-sm-4"><label>Environnement de travail</label></div>
                         <div className="col-sm-8">
                             <div className="form-check">
-                                <input className="form-check-input" type="checkbox" id="gridCheck1" style={{ marginRight: "5px"}} />
+                                <input className="form-check-input" type="checkbox" name="system" id="gridCheck1" value="windows" style={{ marginRight: "5px"}} onClick={this.checkedSystem} />
                                 <label className="form-check-label" htmlFor="gridCheck1">
                                 Windows
                                 </label><br />
-                                <input className="form-check-input" type="checkbox" id="gridCheck1" style={{ marginRight: "5px"}}/>
+                                <input className="form-check-input" type="checkbox" name="system" id="gridCheck2" value="linux" style={{ marginRight: "5px"}} onClick={this.checkedSystem}/>
                                 <label className="form-check-label" htmlFor="gridCheck1">
                                 Linux/Ubuntu
                                 </label><br />
-                                <input className="form-check-input" type="checkbox" id="gridCheck1" style={{ marginRight: "5px"}}/>
+                                <input className="form-check-input" type="checkbox" name="system" id="gridCheck3" value="mac" style={{ marginRight: "5px"}}onClick={this.checkedSystem}/>
                                 <label className="form-check-label" htmlFor="gridCheck1">
                                 MAC OS
                                 </label><br />
-                                <div style={{display: "flex"}}>
-                                    <input className="form-check-input" type="checkbox" id="gridCheck1" style={{ marginRight: "5px"}}/>
-                                    <input type="text" className="form-control" style={{ width: "200px"}} placeholder="Autre" id="input-h"/>
-                                </div>
+                               
                             </div>
                         </div>
                     </div>
 
-                    {/* <div className="form-group row">
-                        <label className="col-sm-4">Technos et outils</label>
-                        <div className="col-sm-8">
-                        <textarea name="toolsInput" value={this.state.toolsInput} onChange={this.handleChange} className="form-control" style={{height: "100px"}} placeholder="Séparez par une virgule"/>
-                        </div>
-                    </div> */}
-
                     <div className="form-group row">
-                        <label className="col-sm-4">Mots-clés</label>
+                        <label className="col-sm-4">Langages/Outils/Compétences</label>
                         <div className="col-sm-8">
-                        <textarea name="keywordsInput" value={this.state.keywordsInput} onChange={this.handleChange} className="form-control" style={{height: "100px"}} placeholder="Séparez par une virgule"/>
+                        <textarea name="keywordsInput" value={this.state.keywordsInput} onChange={this.handleChange} className="form-control" style={{height: "100px"}} placeholder="Exemple: Javascript, Intégration, Responsive design...(Séparez par une virgule)"/>
                         </div>
                     </div>
 
--- src/Components/Login-modal.js
+++ src/Components/Login-modal.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class LoginModal extends Component {
     constructor(){
@@ -18,13 +19,16 @@ class LoginModal extends Component {
                     <div className="form-group">
                         <label htmlFor="exampleInputEmail1">Email address</label>
                         <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
                     <div className="form-group">
                         <label htmlFor="exampleInputPassword1">Password</label>
                         <input type="password" className="form-control" id="exampleInputPassword1" />
+                        <small>Pour la demo, cliquez sur le bouton Connexion pour accéder à votre profil.</small>
                     </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <Link to="/dashboard">
+                        <button onClick={this.props.hideModal} type="submit" className="btn">Connexion</button>
+                    </Link>
+                    
                 </form>
             </div>
         )
